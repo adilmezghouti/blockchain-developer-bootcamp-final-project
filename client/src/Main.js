dc07ff87c6cb243801a1e1498c26da99f2342b22
Adil Mezghouti <adilmezghouti@gmail.com>
2022-01-07 14:55:31 -0500

    Added a title

--- client/src/Main.js
+++ client/src/Main.js
@@ -6,6 +6,9 @@ import VerticalTabs from "./VerticalTabs";
 import {injected} from "./connectors";
 import useInactiveListener from "./hooks/useInactiveListener";
 import useEagerConnect from "./hooks/useEagerConnect";
+import Button from "@mui/material/";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 const Main = () => {
   const { active, error, activate  } = useWeb3React();
@@ -23,6 +26,26 @@ const Main = () => {
   }
 
   return <Container fixed>
+    <Box
+      sx={{
+        flexGrow: 1,
+        display: 'inline-flex',
+        flexWrap: 'wrap',
+        gap: '12px',
+        flexDirection: 'row',
+        justifyItems: 'stretch',
+        alignItems: 'flex-start',
+        border: '1px solid #EFEFEF',
+        marginTop: '20px',
+        marginBottom: '20px',
+        padding: '10px',
+        justifySelf: 'center'
+      }}
+    >
+      <Typography variant="h6" component="div" gutterBottom>
+        FAMILY TRUST - INVEST IN THE FUTURE OF YOUR FAMILY
+      </Typography>
+    </Box>
     <Header />
     <VerticalTabs />
   </Container>
