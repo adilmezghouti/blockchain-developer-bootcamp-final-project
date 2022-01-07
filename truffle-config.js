dad102332c9645c0990dbdf5017167f626eed654
Adil Mezghouti <adilmezghouti@gmail.com>
2022-01-07 12:34:18 -0500

    Added logic to refresh info based on blocks

--- truffle-config.js
+++ truffle-config.js
@@ -7,8 +7,9 @@ module.exports = {
 
   networks: {
     development: {
-      provider: () => new HDWalletProvider(process.env.MNEMONIC, `http://127.0.0.1:8545`),
-      network_id: "*",
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
     },
     ropsten: {
       provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`),
